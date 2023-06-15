2ad9b18def6485b2ee881f72ec91d6b279d98929
laura Muleke <lauramuleke@gmail.com>
2023-06-15 13:57:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints a line with puts function
  * Return : 0 (success)
  */
-int main (void)
+int main(void)
 {
 puts("\"programming is like bulding a multilingual puzzle");
 return (0);
