58a4ffe4e95b8db9efeae918a3ca109c504e1dc0
laura Muleke <lauramuleke@gmail.com>
2023-06-20 16:05:27 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: The number to be checked.
- * Return: 1 for positive number, -1 for negative number or zero for 
- * anything else.
+ * Return: 1 for positive number, -1 for negative number or zero.
  */
 int print_sign(int n)
 {
