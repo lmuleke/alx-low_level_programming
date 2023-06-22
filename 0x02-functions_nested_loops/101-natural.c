65b4487dbccca9a3b1c7a4b3d859c60c0be5f8d8
laura Muleke <lauramuleke@gmail.com>
2023-06-21 17:30:19 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,13 +14,13 @@ int main(void)
 {
 int i, sum = 0;
 
-for (i = 0; i < 1024; 1++)
+for (i = 0; i < 1024; i++)
 {
 if ((i % 3) == 0 || (i % 5) ==0)
 sum += i;
 }
 
-printf(%d\n", sum);
+printf("%d\n", sum);
 
 return (0);
 }
