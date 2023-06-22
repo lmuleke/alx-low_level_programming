157923a099cc7ac4fdc4647035f158a24cdef066
laura Muleke <lauramuleke@gmail.com>
2023-06-21 17:36:31 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,7 +6,7 @@
 
 /**
  * main - lists all the natural numbers below 1024 (excluded)
- *  	  that are multiples of 3 or 5.
+ * that are multiples of 3 or 5.
  *
  * Return: Always 0.
  */
@@ -16,7 +16,7 @@ int i, sum = 0;
 
 for (i = 0; i < 1024; i++)
 {
-if ((i % 3) == 0 || (i % 5) ==0)
+if ((i % 3) == 0 || (i % 5) == 0)
 sum += i;
 }
 
