31860c6c5b0b0424187164c448dfdaf7cf602b24
laura Muleke <lauramuleke@gmail.com>
2023-07-18 15:15:19 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - name of the file is printed
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	printf("compiled from the file: %s\n", __FILE__);
+	return (0);
+}
