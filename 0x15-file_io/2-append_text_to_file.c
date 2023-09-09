53a377fbe68f4c1f733ade7e2e784fc97aa32041
Laura muleke <lauramuleke@gmail.com>
2023-09-09 04:31:35 +0300

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	W = write(o, text_content, len);
+	w = write(o, text_content, len);
 
 	if(o == -1 || w == -1)
 		return (-1);
